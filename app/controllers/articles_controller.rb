f2e982c3a14162bb4047be2b0356e726f4c72a9b
Christopher Soden <karavol27@gmail.com>
2016-04-06 23:34:03 -0600

    completed basic CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,50 @@
+
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
   def index
     @articles = Article.all
   end
 
   def new
-
+    @article = Article.new
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article = Article.find_by(id: params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    # @article.title = params[:article][:title]
+    # @article.body = params[:article][:body]
+    @article.save
+
+    flash.notice = "Article '#{@article.title}' Created!"
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    article = Article.find_by(id: params[:id])
+    flash.notice = "Article '#{article.title}' Deleted!"
+    article.destroy
+
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find_by(id: params[:id])
+  end
+
+  def update
+    @article = Article.find_by(id: params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
   end
 
 end
