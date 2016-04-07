aefb5044bb30a801397cefa1de08d8a0eb3d382c
Christopher Soden <karavol27@gmail.com>
2016-04-06 21:49:51 -0600

    added a few views and a canned stylesheet

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@
 ActiveRecord::Schema.define(version: 20160406212442) do
 
   create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
